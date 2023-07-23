177e2a3b750046325e91a17543490e54b82b1ee2
YashrajPawar <yashrajpawar33@gmail.com>
2023-07-23 15:03:18 +0530

    created the Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <h2 className="FooterTitle"> UpGrad FrontEnd Hackathon</h2>
+        </div>
+    )
+}
+
+export default Footer
